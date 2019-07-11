fc2c389d3dca2cf7203277e681469434a8eed108
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function writeCards(names) {
   let messages = [];
   while(names != []) {
+    let message = "Thank you, " + names[0] + ", for the wonderful suprise gift!";
+    messages.push(message);
     names.shift();
   }
   return messages;
