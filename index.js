9e88caa7b803370f356c4d87ea128e5756f8370d
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:16:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(names) {
   let messages = [];
-  while(names != []) {
+  while(names) {
     let message = "Thank you, " + names[0] + ", for the wonderful suprise gift!";
     messages.push(message);
     names.shift();
