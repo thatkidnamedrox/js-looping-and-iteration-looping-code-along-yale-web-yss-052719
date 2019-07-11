5f04d3ece8d32cc39abe1d66f15401ce99dc5873
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-11 14:28:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, type) {
+
+  let messages = [];
+
+  let i = 0;
+  for( let i = 0; i < names.length; i += 1 ) {
+    let message = "Thank you, " + names[i] + ", for the wonderful " + type + " gift!";
+    messages.push(message);
+  }
+
+  return messages;
+
+}
+
+function countdown(count) {
+  let times = count;
+  while(times >= 0) {
+    console.log(times);
+    times -= 1;
+  }
+}
